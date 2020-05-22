be9ea913620f050b9005c8c99e9af89e6b3abf62
phil-fly <dpf891x@163.com>
2020-05-22 15:36:11 +0800

    修复ssh

--- tool/gossh.go
+++ tool/gossh.go
@@ -8,6 +8,23 @@ import (
 	"net"
 	"time"
 )
+const chacha20Poly1305ID = "chacha20-poly1305@openssh.com"
+const (
+	gcmCipherID    = "aes128-gcm@openssh.com"
+	aes128cbcID    = "aes128-cbc"
+	tripledescbcID = "3des-cbc"
+)
+
+var supportedCiphers = []string{
+	"aes128-ctr", "aes192-ctr", "aes256-ctr",
+	"aes128-gcm@openssh.com",
+	chacha20Poly1305ID,
+	"arcfour256", "arcfour128", "arcfour",
+	aes128cbcID,
+	tripledescbcID,
+	gcmCipherID,
+	"aes128-cbc",
+}
 
 func SshConnect(user, password, host string, port int) (string, error) {
 	var (
@@ -28,7 +45,7 @@ func SshConnect(user, password, host string, port int) (string, error) {
 		Timeout: 6 * time.Second,
 		//2019.6.18  golang默认配置加密方式不包括aes128-cbc  连接交换机需要使用aes128-cbc
 		Config: ssh.Config{
-			Ciphers: []string{"aes128-cbc"},
+			Ciphers: supportedCiphers,
 		},
 		HostKeyCallback: func(hostname string, remote net.Addr, key ssh.PublicKey) error {
 			return nil
